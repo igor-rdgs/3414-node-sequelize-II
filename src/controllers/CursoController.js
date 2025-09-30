21538356d1a83439ac8ae775a20c86186c38d96d
Igor Rodrigues <igor.aguiar1@live.com>
2025-09-30 13:22:26 -0300

    feat: add methods and new routes

--- src/controllers/CursoController.js
+++ src/controllers/CursoController.js
@@ -1,11 +1,31 @@
 const Controller = require('./Controller.js');
 const CursoServices = require('../services/CursoServices.js');
+const { Op } = require('sequelize');
 
 const cursoServices = new CursoServices();
 
 class CursoController extends Controller {
   constructor() {
     super(cursoServices);
+  }
+
+  async pegaCursos(req, res) {
+    const { data_inicial, data_final } = req.query;
+    const where = {};
+
+    //se existirem os params, criar uma prop {}
+    data_inicial || data_final ? where.data_inicio = {} : null;
+    //se existir data inicial, adiciona a prop gte
+    data_inicial ? where.data_inicio[Op.gte] = data_inicial : null;
+    //se existir data final, idem
+    data_final ? where.data_inicio[Op.lte] = data_final : null;
+
+    try {
+      const listaCursos = await cursoServices.pegaTodosOsRegistros(where);
+      return res.status(200).json(listaCursos);
+    } catch (erro) {
+      res.status(500).json({ erro: erro.message });
+    }
   }
 }
 
